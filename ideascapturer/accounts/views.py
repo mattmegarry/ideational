bd4dce3a6060477a2f63a8b7ec87b2598983476d
Matt Megarry <mattmegarry@gmail.com>
2021-12-28 16:06:38 +0000

    Toy login page, seprately hosted, with CORS issues to solve...

--- ideascapturer/accounts/views.py
+++ ideascapturer/accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework import permissions
 
-# Create your views here.
+
+class Test(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request):
+        return HttpResponse({"some": "data"})
